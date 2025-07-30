3be1f600f08dfed5dd13eca5b11e3c6ef0c820c6
GuillemotClement <guillemot.clement@protonmail.com>
2025-07-30 15:28:46 +0200

    feat: create basic CLI

---
+++ src/cli.php
@@ -0,0 +1,18 @@
+#! /user/bin/env php 
+<?php
+// la première ligne est un shebang qui permet d'executer le fichier en tant que script PHP
+// php src/cli.php <commande> <arg> <arg2> => permet de lancer le script
+require __DIR__ . '/../vendor/autoload.php';
+
+// $argv est une variable globale. Contient les arguments passé dans la ligne de commande
+
+// on récupérer les valeurs depuis la variable globale. Si elle n'existe pas, on passe null.
+$command = $argv[1] ?? null;
+$arg1 = $argv[2] ?? null;
+$arg2 = $argv[3] ?? null;
+
+echo "Commande :" . $command . PHP_EOL;
+echo "Argument 1 :" . $arg1 . PHP_EOL;
+echo "Argument 2 :" . $arg2 . PHP_EOL;  
+
+// on viens ensuite choper la commande et appeller la méthode correspondantes.
